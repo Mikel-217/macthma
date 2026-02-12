e3a6efe06771ed6d178b52bcb1717a0b8657d87e
Mikel-217 <Mikel.Kunze887@web.de>
2026-02-12 12:12:05 +0100

    Changes to main.go

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"mikel-kunze.com/matchma/user"
 )
 
 func main() {
 	mux := http.NewServerMux()
 
 	fmt.Println("Server listening on http://localhost:8080/")
+
+	mux.HandleFunc("/login", user.UserLoginHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
