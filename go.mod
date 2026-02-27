18d106c741da17c154525363e19c004e68f2ad45
Mikel-217 <Mikel.Kunze887@web.de>
2026-02-27 11:58:29 +0100

    Renamed handler | Added gorilla mux websocket | Added new struct

--- go.mod
+++ go.mod
@@ -10,4 +10,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.2
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- user/user_match_handler.go
+++ user/user_match_handler.go
@@ -1,8 +1,33 @@
 package user
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"mikel-kunze.com/matchma/logging"
+)
+
+type ConnectedUser struct {
+	UserId      uint
+	Conn        *websocket.Conn
+	IsConnected bool
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 // TODO: upgrade connection to a websocket
 func HandlePlayerJoin(w http.ResponseWriter, r *http.Request) {
 
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		logging.Log(Logging.Error, err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 }
+
+func handleWebsocketConns(ws *websocket.Conn) {}
