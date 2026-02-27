9b2e2c868c7e367e27fcba306376f02bc0056497
Mikel-217 <Mikel.Kunze887@web.de>
2026-02-27 12:40:06 +0100

    Added new structs | Added new func | Added todos

--- user/user_match_handler.go
+++ user/user_match_handler.go
@@ -7,27 +7,41 @@ import (
 	"mikel-kunze.com/matchma/logging"
 )
 
-type ConnectedUser struct {
-	UserId      uint
-	Conn        *websocket.Conn
-	IsConnected bool
+// TODO: restructure!!
+
+type WSServer struct {
+	clients          map[*Client]bool
+	broadcast        chan []byte
+	registerClient   chan *Client
+	unregisterClient chan *Client
+}
+
+type Client struct {
+	UserId uint
+	Conn   *websocket.Conn
+	Send   chan []byte
 }
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-// TODO: upgrade connection to a websocket
+// TODO
+func CreateNewWSServer() *WSServer {
+	return &WSServer{}
+}
+
 func HandlePlayerJoin(w http.ResponseWriter, r *http.Request) {
 
 	ws, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
-		logging.Log(Logging.Error, err.Error())
+		logging.Log(logging.Error, err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
+	go handleWebsocketConns(ws)
 }
 
 func handleWebsocketConns(ws *websocket.Conn) {}
