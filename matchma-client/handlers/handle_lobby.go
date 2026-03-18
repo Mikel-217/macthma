15a0840fcd06e5fe42dac59bf838b835e2dd8be6
Mikel <mikel.kunze@protonmail.com>
2026-03-18 11:24:45 +0100

    Added login logic to client | Added websocket logic to client

--- matchma-client/handlers/handle_lobby.go
+++ matchma-client/handlers/handle_lobby.go
@@ -1,5 +1,62 @@
 package handlers
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+
+	"github.com/gorilla/websocket"
+)
+
 func HandleLobby() {
 
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/join-match"}
+
+	header := make(http.Header)
+	header.Add("Authorization", "Baerer "+AccesToken)
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), header)
+
+	if err != nil {
+		log.Println(err.Error())
+		HandleUserInput()
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	done := make(chan struct{})
+
+	go func() {
+		defer conn.Close()
+		for {
+			_, msg, err := conn.ReadMessage()
+
+			if err != nil {
+				log.Fatal("Read-error", err)
+				break
+			}
+
+			fmt.Println("msg from server: ", string(msg))
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("User requested closing")
+
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+	}
 }
--- matchma-client/handlers/handle_login.go
+++ matchma-client/handlers/handle_login.go
@@ -1,5 +1,50 @@
 package handlers
 
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func HandleLogin() string {
-	return ""
+
+	var userName, pw string
+
+	fmt.Println("Please enter your username:")
+
+	if _, err := fmt.Scan(&userName); err != nil {
+		fmt.Println("Please enter valid credentials")
+		HandleLogin()
+	}
+
+	fmt.Println("Please enter your password")
+
+	if _, err := fmt.Scan(&pw); err != nil {
+		fmt.Println("Please enter valid credentials")
+		HandleLogin()
+	}
+
+	headerVal := base64.StdEncoding.EncodeToString([]byte(userName + ":" + pw))
+
+	// build the request
+	req, _ := http.NewRequest("POST", UrlLogin, nil)
+	req.Header.Add("Authorization", "Basic: "+headerVal)
+
+	// send the request
+	response, err := http.DefaultClient.Do(req)
+
+	if err != nil || response.StatusCode != 200 {
+		fmt.Println(err.Error())
+		HandleUserInput()
+	}
+
+	bodyData, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		HandleUserInput()
+	}
+
+	return string(bodyData)
 }
